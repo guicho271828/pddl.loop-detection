3f3801812e90d2f776fcf3a4410a31da7c9fa923
guicho <guicho@guicho-ThinkPad-X60.(none)>
2013-07-23 17:28:25 +0900

    included utility function

--- t/pddl.loop-detection.lisp
+++ t/pddl.loop-detection.lisp
@@ -15,6 +15,24 @@
   (:shadowing-import-from :fiveam :fail))
 (in-package :pddl.loop-detection-test)
 
-(def-suite :pddl)
-(in-suite :pddl)
+(def-suite :pddl.loop-detection :in :pddl)
+(in-suite :pddl.loop-detection)
+
+(defun data (name)
+  (merge-pathnames
+   name
+   (asdf:system-relative-pathname
+    :pddl.loop-detection "data/")))
+
+(defvar +problem+ (data "pfile1"))
+(defvar +domain+ (data "domain.pddl"))
+(defvar +plan+ (data "pfile1.plan.1"))
+
+(print +problem+)
+(print +domain+)
+(print +plan+)
+
+(defvar domain)
+(defvar problem)
+(defvar plan)
 
