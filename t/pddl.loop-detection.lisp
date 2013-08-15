447ad58c518a55e27dd17d0e6f110faaf7b6a20c
guicho <guicho@guicho-ThinkPad-X60.(none)>
2013-08-15 16:31:17 +0900

    updated roadmap

--- t/pddl.loop-detection.lisp
+++ t/pddl.loop-detection.lisp
@@ -18,27 +18,3 @@
 (def-suite :pddl.loop-detection :in :pddl)
 (in-suite :pddl.loop-detection)
 
-(defun data (name)
-  (merge-pathnames
-   name
-   (asdf:system-relative-pathname
-    :pddl.loop-detection "data/")))
-
-(defvar +domain+ (data "domain.pddl"))
-(defvar +problem+ (data "p1.pddl"))
-(defvar +plan+ (data "p1.plan.1"))
-
-(defvar +c-domain+ (data "domain-with-costs.pddl"))
-(defvar +c-problem+ (data "model2b1c.pddl"))
-(defvar +c-plan+ (data "model2b1c.plan.1"))
-
-(print +problem+)
-(print +domain+)
-(print +plan+)
-
-(test parse-domain
-  (finishes (parse-file +domain+)))
-
-(test parse-problem
-  (finishes (parse-file +problem+)))
-
