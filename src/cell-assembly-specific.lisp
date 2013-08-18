555dada7c7ec3e0b1625846023dfc60b3c1d289e
guicho <guicho@guicho-ThinkPad-X60.(none)>
2013-08-19 04:07:22 +0900

    better

--- src/cell-assembly-specific.lisp
+++ src/cell-assembly-specific.lisp
@@ -30,17 +30,17 @@
 		      nil
 		      nil))))
 	(for pinfo previous info)
-	(for ppinfo previous info back 2)
 	(collecting info)
-	(unless pinfo
-	  (next-iteration))
 	
-	(setf (fourth pinfo) (first info)) ; set end-time information
+	;; (unless pinfo
+	;;   (next-iteration))
+	(when pinfo
+	  (setf (fourth pinfo) (first info))); set end-time information
 	(setf
-	 (third pinfo)
-	 (%extract-base-position (object *problem* base) info pinfo ppinfo))))
+	 (third info)
+	 (%extract-base-position (object *problem* base) info pinfo))))
 
-(defun %extract-base-position (base info pinfo ppinfo)
+(defun %extract-base-position (base info pinfo)
   (flet ((base-related (preds)
 	   (remove-if-not (curry #'%appears base) preds))
 	 (not-base-related (preds)
@@ -48,34 +48,27 @@
     (let* ((action (second info))
 	   (d (delete-list action))
 	   (a (add-list action))
-	   (p (positive-preconditions action))
-	   (paction (second pinfo))
-	   (pd (delete-list paction))
-	   (pa (add-list paction))
-	   (pp (positive-preconditions paction)))
-      @ignorable action d a p paction pd pa pp
+	   (p (positive-preconditions action)))
+      @ignorable action d a p 
       (remove-if-not
        (lambda (obj)
 	 (labels ((in (list) (some (curry #'%appears obj) list))
 		  (out (list) (not (in list))))
 	   ;; add rules here!	     
 	   ;; とりあえず場所の指示子は一つとして考える
-	   (and (in (base-related pa))
+	   (and (in (base-related a))
 		;;(base-related pd)
-		(if (third ppinfo)
+		(if (and pinfo (third pinfo))
 		    (some
 		     (lambda (pred)
 		       (subsetp
-			(third ppinfo)
 			(remove-if
 			 (curry #'eqname base)
-			 (parameters pred))))
-		     (base-related pd))
-		    t)
-		)))
-       (intersection
-	(remove-if (curry #'eqname base) (parameters action))
-	(remove-if (curry #'eqname base) (parameters paction)))))))
+			 (parameters pred))
+			(third pinfo)))
+		     (base-related d))
+		    t))))
+       (remove-if (curry #'eqname base) (parameters action))))))
 
 
 @export
