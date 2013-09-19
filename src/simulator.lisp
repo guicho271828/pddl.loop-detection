92f97955ea0aba410ac905c73a60f7c9f3bea09a
Masataro Asai <guicho2.71828@gmail.com>
2013-09-19 15:35:39 +0900

    refactoring: %loop-verbosity is now an inlined function.

--- src/simulator.lisp
+++ src/simulator.lisp
@@ -196,18 +196,6 @@ the number of bases.
       (find (first-elt path2) path1 :test #'equalp)
       (find (last-elt path2) path1 :test #'equalp)))
 
-@export
-(defun exploit-loopable-steady-states (movements-shrinked steady-states
-                                       &key (verbose t))
-  "Returns the list of solution path from start-of-loop to end-of-loop.
-start-of-loop is always the same list to the steady-state in the
-meaning of EQUALP."
-  
-  (case verbose
-    ((2 t)       (%verbose movements-shrinked steady-states))
-    ((1 :modest) (%modest movements-shrinked steady-states))
-    (otherwise   (%none movements-shrinked steady-states))))
-
 (defun %merge-buckets (buckets1 buckets2)
   (map 'vector
        (lambda (bucket1 bucket2)
@@ -219,92 +207,153 @@ meaning of EQUALP."
 
 (defun %fast-failure-check (ss bucket)
   (some (rcurry #'%constraint< ss) bucket))
-  
-(macrolet ((%loop-verbosity (before
-                             after-success
-                             after-failure
-                             after-duplication
-                             search-argument)
-             
-             `(let* ((m-num (length moves))
-                     (max (length steady-states))
-                     (buckets (make-buckets m-num))
-                     (bucket-locks (make-array m-num))
-                     (failure-buckets (make-buckets m-num))
-                     (failure-buckets-locks (make-buckets m-num)))
-                @ignorable max
-                (dotimes (j m-num)
-                  (setf (aref failure-buckets-locks j) (make-lock))
-                  (setf (aref bucket-locks j) (make-lock)))
-                (flet ((%exploit/thread (ss)
-                         (let ((bases (1- (length ss))))
-                           ,before
-                           (cond
-                             ((%forward-duplication-check ss
-                                                          (make-eol ss m-num)
-                                                          (aref buckets bases))
-                              ,after-duplication)
-                             ((iter (for bucket in-vector failure-buckets below bases)
-                                    (thereis (%fast-failure-check ss bucket)))
-                              ,after-failure)
-                             (t
-                              (if-let ((result
-                                        (search-loop-path
-                                         moves ss :verbose ,search-argument)))
-                                (progn
-                                  ,after-success
-                                  (with-lock-held ((aref bucket-locks bases))
-                                    (push result (aref buckets bases))))
-                                (progn
-                                  ,after-failure
-                                  (with-lock-held ((aref failure-buckets-locks bases))
-                                    (push ss (aref failure-buckets bases)))
-                                  (iter (for bucket
-                                             in-vector failure-buckets
-                                             with-index j above bases)
-                                        (with-lock-held ((aref failure-buckets-locks j))
-                                          (setf (aref failure-buckets j)
-                                                (remove-if (curry #'%constraint< ss)
-                                                           (aref failure-buckets j))))))))))))
-                  (pmap nil
-                        #'%exploit/thread
-                        (shuffle (coerce steady-states 'vector)))
-                  (reduce #'append (%post-duplication-check buckets) :from-end t)))))
-  
-  
-  (defun %verbose (moves steady-states)
-    (let ((i 0) (i-lock (make-lock "i")))
+
+(progn
+  (declaim (inline funcall-if-functionp))
+  (defun funcall-if-functionp (fn &rest arguments)
+    (when (functionp fn)
+      (apply fn arguments)))
+
+  (declaim (inline %loop-verbosity))
+  (defun %loop-verbosity (before
+                          after-success
+                          after-failure
+                          after-duplication
+                          forward-duplication-check-p
+                          post-duplication-check-p
+                          steady-states
+                          moves
+                          verbose)
+    (let* ((m-num (length moves))
+           (max (length steady-states))
+           (buckets (make-buckets m-num))
+           (bucket-locks (make-array m-num))
+           (failure-buckets (make-buckets m-num))
+           (failure-buckets-locks (make-buckets m-num)))
+      @ignorable max
+      (dotimes (j m-num)
+        (setf (aref failure-buckets-locks j) (make-lock))
+        (setf (aref bucket-locks j) (make-lock)))
+      (flet ((%exploit/thread (ss)
+               (let ((bases (1- (length ss))))
+                 (funcall-if-functionp before)
+                 (cond
+                   ((and forward-duplication-check-p
+                         (%forward-duplication-check ss
+                                                     (make-eol ss m-num)
+                                                     (aref buckets bases)))
+                    (funcall-if-functionp after-duplication ss))
+                   ((iter (for bucket in-vector failure-buckets below bases)
+                          (thereis (%fast-failure-check ss bucket)))
+                    (funcall-if-functionp after-failure))
+                   (t
+                    (if-let ((result
+                              (search-loop-path
+                               moves ss :verbose verbose)))
+                      (progn
+                        (funcall-if-functionp after-success)
+                        (with-lock-held ((aref bucket-locks bases))
+                          (push result (aref buckets bases))))
+                      (progn
+                        (funcall-if-functionp after-failure)
+                        (with-lock-held ((aref failure-buckets-locks bases))
+                          (push ss (aref failure-buckets bases)))
+                        (iter (for bucket
+                                   in-vector failure-buckets
+                                   with-index j above bases)
+                              (with-lock-held ((aref failure-buckets-locks j))
+                                (setf (aref failure-buckets j)
+                                      (remove-if (curry #'%constraint< ss)
+                                                 (aref failure-buckets j))))))))))))
+        (pmap nil
+              #'%exploit/thread
+              (shuffle (coerce steady-states 'vector)))
+        (reduce #'append (if post-duplication-check-p
+                             (%post-duplication-check buckets)
+                             buckets)
+                :from-end t))))
+
+  (defun %verbose (moves steady-states
+                   forward-duplication-check-p
+                   post-duplication-check-p)
+    (let ((i 0)
+          (i-lock (make-lock "i"))
+          (max (length steady-states)))
       (%loop-verbosity
-       (with-lock-held (i-lock)
-         (incf i)
+       (lambda ()
+         (with-lock-held (i-lock)
+           (incf i)
+           (with-lock-held (*print-lock*)
+             (format *shared-output* "~%~a/~a: " i max))))
+       (lambda ()
          (with-lock-held (*print-lock*)
-           (format *shared-output* "~%~a/~a: " i max)))
-       (with-lock-held (*print-lock*)
-         (format *shared-output* " ...success!"))
-       (with-lock-held (*print-lock*)
-         (format *shared-output* " ...failed."))
-       (with-lock-held (*print-lock*)
-         (format t 
-          "~w is not searched because it had appeared in the other loop."
-          ss))
+           (format *shared-output* " ...success!")))
+       (lambda ()
+         (with-lock-held (*print-lock*)
+           (format *shared-output* " ...failed.")))
+       (lambda (ss)
+         (with-lock-held (*print-lock*)
+           (format *shared-output*
+                   "~w is not searched because it had appeared in the other loop."
+                   ss)))
+       forward-duplication-check-p
+       post-duplication-check-p
+       steady-states
+       moves
        t)))
 
-  (defun %modest (moves steady-states)
+  (defun %modest (moves steady-states
+                  forward-duplication-check-p
+                  post-duplication-check-p)
     (let ((i 0) (i-lock (make-lock "i")))
       (%loop-verbosity
-       (with-lock-held (i-lock)
-         (incf i)
-         (when (zerop (mod i 60))
-           (with-lock-held (*print-lock*)
-             (terpri *shared-output*))))
-       (with-lock-held (*print-lock*)
-         (write-char #\. *shared-output*))
-       (with-lock-held (*print-lock*)
-         (write-char #\F *shared-output*))
-       (with-lock-held (*print-lock*)
-         (write-char #\D *shared-output*))
+       (lambda ()
+         (with-lock-held (i-lock)
+           (incf i)
+           (when (zerop (mod i 60))
+             (with-lock-held (*print-lock*)
+               (terpri *shared-output*)))))
+       (lambda ()
+         (with-lock-held (*print-lock*)
+           (write-char #\. *shared-output*)))
+       (lambda ()
+         (with-lock-held (*print-lock*)
+           (write-char #\F *shared-output*)))
+       (lambda ()
+         (with-lock-held (*print-lock*)
+           (write-char #\D *shared-output*)))
+       forward-duplication-check-p
+       post-duplication-check-p
+       steady-states
+       moves
        nil)))
 
-  (defun %none (moves steady-states)
-    (%loop-verbosity nil nil nil nil nil)))
+  (defun %none (moves steady-states
+                forward-duplication-check-p
+                post-duplication-check-p)
+    (%loop-verbosity nil nil nil nil
+                     forward-duplication-check-p
+                     post-duplication-check-p
+                     steady-states
+                     moves
+                     nil))
 
+  @export
+  (defun exploit-loopable-steady-states (movements-shrinked steady-states
+                                         &key (verbose t) (duplication-check t))
+    "Returns the list of solution path from start-of-loop to end-of-loop.
+start-of-loop is always the same list to the steady-state in the
+meaning of EQUALP."
+    (declare (optimize (speed 3)))
+    (declare (inline %verbose
+                     %modest
+                     %none))
+    (macrolet ((%call (name) `(case duplication-check
+                                (nil           (,name movements-shrinked steady-states nil nil))
+                                (:post-only    (,name movements-shrinked steady-states nil t))
+                                (:forward-only (,name movements-shrinked steady-states t nil))
+                                (otherwise     (,name movements-shrinked steady-states t t)))))
+      (case verbose
+        ((2 t)       (%call %verbose))
+        ((1 :modest) (%call %modest))
+        (otherwise   (%call %none))))))
